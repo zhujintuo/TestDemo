5a7dd9d882dfb89b84f611a436b18ac8784ada04
zhujintuo <942408056@qq.coom>
2015-12-16 21:56:28 +0800

    add 3.7

--- NodeJSTest/Chapter3/3.4/session.js
+++ NodeJSTest/Chapter3/3.4/session.js
@@ -1,9 +1,54 @@
 var start=function(res,req)
 {
+	var conn={res:res,req:req};
+	var cookies={};
+	
+	if(typeof conn.req.headers.cookie!=="undefined"){
+		conn.req.headers.cookie.split(';').forEach(function(cookie){
+			var parts=cookie.split('=');
+			cookies[parts[0].trim()]=(parts[1]||'').trim();
+		});
+	}else{
+		cookies.SESSID=0;
+	}
+	
+	var SESSID=cookies.SESSID;
+	if(typeof sessions[SESSID]!=="undefined"){
+		session=sessions[SESSID];
+		if(session.expires<Date()){
+			delete sessions[sess.SESSID];
+			return newSession(conn.res);
+		}else{
+			var dt=new Date();
+			dt.setMinutes(dt.getMinutes()+30);
+			session.expires=dt;
+			return sessions[SESSID];
+		}
+	}
 }
 
 function newSession(res){
 	var chars="0123456789ABCDEFJHIJKLMNOPQRSTURWXTZabcdefghiklmnopqrstuvwxyz";
+	var SESSID='';
+	for(var i=0;i<40;i++){
+		var rnum =Math.floor(Math.random()*chars.length);
+		SESSID+=chars.substring(rnum, rnum+1);
+	}
+	
+	if(typeof sessions[SESSID]!=="undefined")
+		return newSession(res);
+	
+	var dt=new Date();
+	dt.setMilliseconds(dt.getMinutes()+30);
+	
+	var session={
+			SESSID:SESSID,
+			expires:dt
+	};
+	
+	sessions[SESSID]=session;
+    res.setHeader('Set-Cookie','SESSID='+SESSID);
+    return session;
 }
 
 function cleanSession(){
---
+++ NodeJSTest/Chapter3/3.7/static/js/router.js
@@ -0,0 +1,8 @@
+global.BASE_DIR=__dirname;
+global.APP=BASE_DIR+"/app/";
+global.CON=APP+"/controller/";
+global.CORE=APP+"/core/";
+global.LIB=BASE_DIR+"/node_modules/";
+global.CONF=BASE_DIR+"/conf/";
+global.STATIC=BASE_DIR+"/static/";
+global.VIEW=BASE_DIR+"/view/";
