b2b742aec6ebfb851a21cf751e40a4e1104e32c1
zhujintuo <942408056@qq.coom>
2016-01-18 22:35:24 +0800

    PHPTest

---
+++ Test/Test.Winfrom/ProgressBar/TestProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test.Winfrom.ProgressBar
+{
+    public partial class TestProgressBar : Form
+    {
+        public TestProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
